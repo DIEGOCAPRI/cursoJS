c2eb63bb4ef855e7fe31acedb5ba6446f3785b78
DIEGOCAPRI <54905603+DIEGOCAPRI@users.noreply.github.com>
2019-09-11 08:53:47 -0300

    Add files via upload

--- jsEntradaSalida-7.js
+++ jsEntradaSalida-7.js
@@ -3,22 +3,46 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{var numuno;
+    var numdos;
+    var resultado;
+    numuno = parseInt (document.getElementById("numeroUno").value);
+    numdos = parseInt (document.getElementById("numeroDos").value);
+    resultado = numuno + numdos;
+    alert ("El resultado es " + resultado);	
 	
 }
 
 function restar()
-{
+{var numuno;
+    var numdos;
+    var resultado;
+    numuno = parseInt (document.getElementById("numeroUno").value);
+    numdos = parseInt (document.getElementById("numeroDos").value);
+    resultado = numuno - numdos;
+    alert ("El resultado es " + resultado);	
 	
 }
 
 function multiplicar()
-{ 
+{ var numuno;
+    var numdos;
+    var resultado;
+    numuno = parseInt (document.getElementById("numeroUno").value);
+    numdos = parseInt (document.getElementById("numeroDos").value);
+    resultado = numuno * numdos;
+    alert ("El resultado es " + resultado);	
 	
 }
 
 function dividir()
-{
+{var numuno;
+    var numdos;
+    var resultado;
+    numuno = parseInt (document.getElementById("numeroUno").value);
+    numdos = parseInt (document.getElementById("numeroDos").value);
+    resultado = numuno / numdos;
+    alert ("El resultado es " + resultado);	
 	
 }
 
